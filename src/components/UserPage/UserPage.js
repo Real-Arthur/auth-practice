5f1c4e5061ddaf9004025f230bcf82a2225e3dbb
Tyler Jorenby <tyler.jorenby@gmail.com>
2020-10-09 10:23:12 -0500

    Register User component is functioning. Able to log in after registering

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -3,14 +3,19 @@ import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
+
 class UserPage extends Component {
 
   // this component doesn't do much to start, just renders some user info to the DOM
+
+
+
   render(props) {
+    console.log(this.props.store);
     return (
       <div>
-        <h1 id="welcome">Welcome, {props.store.user.username}!</h1>
-        <p>Your ID is: {props.store.user.id}</p>
+        <h1 id="welcome">Welcome, {this.props.store.user.username}!</h1>
+        <p>Your ID is: {this.props.store.user.id}</p>
         <LogOutButton className="log-in" />
       </div>
     )
